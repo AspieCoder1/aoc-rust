2c0f71431784c77990a7a9f41cd8eed5be170814
Luke Braithwaite <lukebraithwaite7@gmail.com>
2025-12-31 17:15:34 +0000

    Add solutions for aoc 2024 day 6 part 2

--- src/year2024/day06.rs
+++ src/year2024/day06.rs
@@ -15,17 +15,17 @@ pub fn main(input_data: &str) -> Result<(usize, usize)> {
 
 fn part1(input: &Grid<char>) -> usize {
     let start_pos = input.all_positions(|&c| c == '^').next().unwrap();
-    let mut visited_position = HashSet::from([(start_pos.0, start_pos.1)]);
+    get_visited_location(&input, (start_pos.0, start_pos.1)).len()
+}
+
+fn get_visited_location(input: &Grid<char>, start_pos: (usize, usize)) -> HashSet<(usize, usize)> {
+    let mut visited_positions = HashSet::new();
     let mut curr_i = start_pos.0 as isize;
     let mut curr_j = start_pos.1 as isize;
     let mut direction: (isize, isize) = (-1, 0);
 
-    while curr_i >= 0
-        && curr_i < input.height as isize
-        && curr_j >= 0
-        && curr_j < input.width as isize
-    {
-        visited_position.insert((curr_i as usize, curr_j as usize));
+    loop {
+        visited_positions.insert((curr_i as usize, curr_j as usize));
         let new_i = curr_i + direction.0;
         let new_j = curr_j + direction.1;
 
@@ -48,12 +48,60 @@ fn part1(input: &Grid<char>) -> usize {
         curr_i += direction.0;
         curr_j += direction.1;
     }
-
-    visited_position.len()
+    visited_positions
 }
 
-fn part2(_input: &Grid<char>) -> usize {
-    0
+fn check_does_loop(input: &Grid<char>, start_pos: (usize, usize)) -> bool {
+    let mut turns = HashSet::new();
+
+    let mut curr_i = start_pos.0 as isize;
+    let mut curr_j = start_pos.1 as isize;
+    let mut direction: (isize, isize) = (-1, 0);
+
+    loop {
+        let new_i = curr_i + direction.0;
+        let new_j = curr_j + direction.1;
+
+        if new_i < 0 || new_i >= input.height as isize || new_j < 0 || new_j >= input.width as isize
+        {
+            return false;
+        }
+
+        if input[(new_i as usize, new_j as usize)] == '#' {
+            if turns.contains(&(curr_i, curr_j, direction)) {
+                return true;
+            } else {
+                turns.insert((curr_i, curr_j, direction));
+            }
+            // Perform the right turn
+            let new_direction = match direction {
+                (-1, 0) => (0, 1),
+                (0, 1) => (1, 0),
+                (1, 0) => (0, -1),
+                (0, -1) => (-1, 0),
+                _ => panic!("Invalid direction"),
+            };
+            direction = new_direction;
+        }
+        curr_i += direction.0;
+        curr_j += direction.1;
+    }
+}
+
+fn part2(input: &Grid<char>) -> usize {
+    let mut acc = 0;
+    let start_pos = input.all_positions(|&c| c == '^').next().unwrap();
+    let possible_obstruction_locations = input.all_positions(|&c| c == '.');
+
+    for pos in possible_obstruction_locations {
+        let mut new_grid = input.clone();
+        new_grid[(pos.0, pos.1)] = '#';
+
+        if check_does_loop(&new_grid, (start_pos.0, start_pos.1)) {
+            acc += 1;
+        }
+    }
+    acc
 }
 
 #[cfg(test)]
@@ -77,5 +125,11 @@ mod tests {
     fn test_part1() {
         let input = Grid::<char>::from_str(EXAMPLE).unwrap();
         assert_eq!(part1(&input), 41);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = Grid::<char>::from_str(EXAMPLE).unwrap();
+        assert_eq!(part2(&input), 6);
     }
 }

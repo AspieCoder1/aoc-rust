5c1c4b518bbbef8b1a8809009a2165a28bcfef5d
Luke Braithwaite <lukebraithwaite7@gmail.com>
2026-01-01 21:28:34 +0000

    Add solution for 2024 day 9 part 1 using more efficient implementation

--- src/year2024/day09.rs
+++ src/year2024/day09.rs
@@ -3,6 +3,8 @@
 //! Link: <https://adventofcode.com/2024/day/9>
 
 use anyhow::Result;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::iter::repeat_n;
 
 pub fn main(input: &str) -> Result<(usize, usize)> {
@@ -10,42 +12,144 @@ pub fn main(input: &str) -> Result<(usize, usize)> {
     Ok((part1(&data), part2()))
 }
 
-fn parse_input(input: &str) -> Result<Vec<isize>> {
-    let mut id = 0_isize;
-    let mut data_block: Vec<isize> = Vec::new();
+fn parse_input(input: &str) -> Result<Vec<FileBlock>> {
+    let mut blocks: Vec<FileBlock> = Vec::new();
+    let mut block_idx = 0_usize;
+    let mut file_id = 0_isize;
 
     for (i, c) in input.chars().enumerate() {
-        let digit = c.to_digit(10).unwrap();
-        if i % 2 == 0 {
-            data_block.extend(repeat_n(id, digit as usize));
-            id += 1;
-        } else {
-            data_block.extend(repeat_n(-1, digit as usize));
+        let block_size = c.to_digit(10).unwrap() as usize;
+        let end_idx = block_idx + block_size - 1;
+        if block_size == 0 {
+            continue;
         }
+        if i % 2 == 0 {
+            blocks.push(FileBlock {
+                start: block_idx,
+                end: end_idx,
+                file_id,
+            });
+            file_id += 1;
+        } else {
+            blocks.push(FileBlock {
+                start: block_idx,
+                end: end_idx,
+                file_id: -1,
+            });
+        }
+        block_idx = end_idx + 1;
     }
-    Ok(data_block)
+    blocks.sort();
+    Ok(blocks)
 }
 
-fn part1(input: &[isize]) -> usize {
-    let mut disk = input.to_vec();
+fn part1(input: &[FileBlock]) -> usize {
+    let mut file_blocks = BinaryHeap::new();
+    let mut free_blocks = BinaryHeap::new();
 
-    for i in (0..disk.len()).rev() {
-        let empty_idx = disk.iter().position(|&x| x == -1).unwrap();
-        if i <= empty_idx {
-            break;
+    for block in input {
+        if block.file_id == -1 {
+            free_blocks.push(Reverse(block.clone()));
+        } else {
+            file_blocks.push(block.clone());
         }
-        disk.swap(i, empty_idx);
     }
 
-    disk.iter()
-        .enumerate()
-        .filter(|&(_, &x)| x > 0)
-        .map(|(i, &file_id)| i * file_id as usize)
-        .sum()
+    let mut curr_file = file_blocks.pop().unwrap();
+    while let Some(Reverse(empty_block)) = free_blocks.pop() {
+        let free_space = empty_block.size();
+        let file_size = curr_file.size();
+
+        if curr_file.start < empty_block.start {
+            break;
+        }
+
+        match free_space.cmp(&file_size) {
+            std::cmp::Ordering::Less => {
+                let new_file = FileBlock {
+                    start: empty_block.start,
+                    end: empty_block.end,
+                    file_id: curr_file.file_id,
+                };
+                let mut updated_file = curr_file.clone();
+                updated_file.end -= free_space;
+                curr_file = updated_file;
+                file_blocks.push(new_file);
+            }
+            std::cmp::Ordering::Equal => {
+                // File and empty space are the same size
+                let new_file = FileBlock {
+                    start: empty_block.start,
+                    end: empty_block.end,
+                    file_id: curr_file.file_id,
+                };
+                file_blocks.push(new_file);
+                curr_file = file_blocks.pop().unwrap();
+            }
+            std::cmp::Ordering::Greater => {
+                // Empty space is greater than file size
+                let new_file = FileBlock {
+                    start: empty_block.start,
+                    end: empty_block.start + file_size - 1,
+                    file_id: curr_file.file_id,
+                };
+                let remaining_empty_block = FileBlock {
+                    start: new_file.end + 1,
+                    end: empty_block.end,
+                    file_id: -1,
+                };
+                free_blocks.push(Reverse(remaining_empty_block));
+                file_blocks.push(new_file);
+                curr_file = file_blocks.pop().unwrap();
+            }
+        }
+    }
+    file_blocks.push(curr_file);
+    file_blocks.iter().map(|f| f.check_sum()).sum()
 }
 
 fn part2() -> usize {
     0
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+struct FileBlock {
+    start: usize,
+    end: usize,
+    file_id: isize,
+}
+
+impl FileBlock {
+    fn new(start: usize, end: usize, file_id: isize) -> Self {
+        Self {
+            start,
+            end,
+            file_id,
+        }
+    }
+    fn size(&self) -> usize {
+        self.end.abs_diff(self.start) + 1
+    }
+
+    fn check_sum(&self) -> usize {
+        let (s, e) = (self.start as isize, self.end as isize);
+        let sum_to_end = (e + 1) * e / 2;
+        let sum_to_start = if s < 1 { 0 } else { (s) * (s - 1) / 2 };
+        let check_sum = self.file_id * (sum_to_end - sum_to_start);
+        check_sum as usize
+    }
+}
+
+impl PartialOrd for FileBlock {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for FileBlock {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.start.cmp(&other.start)
+    }
 }
 
 #[cfg(test)]
@@ -54,15 +158,52 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn test_parse_input() {
-        let input = parse_input("2333133121414131402").unwrap();
+    fn test_parse_blocks() {
+        let input = parse_input("12345").unwrap();
         assert_eq!(
             input,
             vec![
-                0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1, 4, 4, -1, 5, 5,
-                5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9
+                FileBlock::new(0, 0, 0),
+                FileBlock::new(1, 2, -1),
+                FileBlock::new(3, 5, 1),
+                FileBlock::new(6, 9, -1),
+                FileBlock::new(10, 14, 2),
             ]
-        );
+        )
+    }
+
+    #[test]
+    fn test_parse_input_2() {
+        let input = parse_input("2333133121414131402").unwrap();
+        let expected_blocks = vec![
+            FileBlock::new(0, 1, 0),
+            FileBlock::new(2, 4, -1),
+            FileBlock::new(5, 7, 1),
+            FileBlock::new(8, 10, -1),
+            FileBlock::new(11, 11, 2),
+            FileBlock::new(12, 14, -1),
+            FileBlock::new(15, 17, 3),
+            FileBlock::new(18, 18, -1),
+            FileBlock::new(19, 20, 4),
+            FileBlock::new(21, 21, -1),
+            FileBlock::new(22, 25, 5),
+            FileBlock::new(26, 26, -1),
+            FileBlock::new(27, 30, 6),
+            FileBlock::new(31, 31, -1),
+            FileBlock::new(32, 34, 7),
+            FileBlock::new(35, 35, -1),
+            FileBlock::new(36, 39, 8),
+            FileBlock::new(40, 41, 9),
+        ];
+        assert_eq!(input, expected_blocks);
+    }
+
+    #[test]
+    fn test_get_file_size() {
+        let block = FileBlock::new(0, 1, 0);
+        let block2 = FileBlock::new(32, 34, 7);
+        assert_eq!(block.size(), 2);
+        assert_eq!(block2.size(), 3);
     }
 
     #[test]
